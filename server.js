57de04c89d519eab357d52bcc111bcdf4192faa7
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-08-03 21:58:03 +0200

    add info and history pages

--- server.js
+++ server.js
@@ -8,34 +8,30 @@ const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.static(path.join(__dirname, '/user')));
 
 // Custom Middleware for /user/ routes
 app.use('/user/', (req, res, next) => {
-  res.show('forbidden.html');
+  res.render('forbidden', { layout: false })
 });
 
 // Rest of paths
 app.get(['/','/home'], (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: false })
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false })
 });
 
 app.get('/hello/:name', (req, res) => {
   res.render('Hello', { layout: false, name: req.params.name });
+});
+
+app.use((req, res) => {
+  res.status(404).render('404', {layout: false});
 });
 
 app.listen(8000, () => {
